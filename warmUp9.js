17aefea204ca90257ca85fc1ff18640d7f2744f8
guizenifarouk <guizeni.farouk95@gmail.com>
2019-10-29 10:02:09 +0100

    warmup10

--- warmUp9.js
+++ warmUp9.js
@@ -8,3 +8,35 @@
 // Create an unordered list with three anchor tags in it
 // Place In the footer a copyright claimer
 // Have Fun
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta CHARSET="UTF-8">
+		<link href="untitled.css" rel="stylesheet" type="text/css">
+		<meta keywords="rbk warmup">
+		<meta author="farouk guizeni">
+		<meta description="warmup number 9 HTML and CSS">
+		<title>First html and css warmup</title>
+	</head>
+	<body background-color="green">
+		<center><h1>Dragon ball super</h1></center>
+		<div>
+		<img src="https://i.pinimg.com/originals/fd/ef/cd/fdefcd21a82a696ea169b243afe3d72c.png" alt="GOKU super instinct picture">
+		<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ477lrQE3Izps-K5ftwfXOyV7n5vxZflFd2_6_-vdaujjc0JgM&s" alt="VAGETA god form picture">
+		<img src="https://s.pacn.ws/640/r2/dragon-ball-super-gigantic-series-beerus-god-of-destruction-487277.6.jpg?ocemyr" alt="BEERUS god of destruction picture">
+	</div>
+	<h2>if you have time and you want to start watching this amazing anime</h2>
+	<button href="http://www.dragonballsuper-vostfr.com/" >clique to watch dragon ball super</button>
+	<h2>if you want to read the manga of dragon ball super</h2>
+	<button href="https://dragonballsuper-france.fr/dragon-ball-super-manga/" >clique to read dragon ball super manga</button>
+	<ul>
+		<li><a href=""></a></li>
+		<li><a href=""></a></li>
+		<li><a href=""></a></li>
+	</ul>
+		
+	</body>
+	<footer>
+		<h6>© copyright claimer</h6>
+	</footer>
+</html>
