4f8a40b9d201f3fa128c474517f72e72997af4f3
guizenifarouk <guizeni.farouk95@gmail.com>
2019-10-30 10:08:32 +0100

    warmup11 out of time

---
+++ wamup11Jquery.js
@@ -0,0 +1,15 @@
+ 
+$('#b1').on('click', function(){
+	if($('#b3') = false){
+	$('#list1').append('<li>new item</li>')
+}
+})
+	
+
+
+$('#b2').on('click', function(){
+	if($('#b3') = false){
+	$('#list2').append('<li>new name</li>')
+}
+})
+
--- warmUp10.js
+++ warmUp10.js
@@ -10,5 +10,6 @@ $('#b1').on('click', function(){
 	count++;
 	}
 	})
+// i tried to use .attr and .addClass but each time the class is overiding , so every time i add an li i add his class at the same time 
 	
 	
