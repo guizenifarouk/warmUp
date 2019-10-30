5cee6ba1a14a328d4e93a13524ecc9707634b2c5
guizenifarouk <guizeni.farouk95@gmail.com>
2019-10-30 08:26:42 +0100

    warmup 10 refactor

--- warmUp10.js
+++ warmUp10.js
@@ -1,3 +1,14 @@
-$('.b1').on('click', function(){
-	$('.txt1').val().($('ul').append('<li></li>'))
-})
+console.log('hi')
+
+var count = 0;
+$('#b1').on('click', function(){							
+	if(count % 2 === 0){					
+	$('#list').append(('<li class ="class1">'+$('#txt1').val()+'</li>'));		
+	count++;													
+	} else {
+	$('#list').append(('<li class ="class2">'+$('#txt1').val()+'</li>'));		
+	count++;
+	}
+	})
+	
+	
