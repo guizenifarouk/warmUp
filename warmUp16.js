76fe802d930567a25b02855d0ee9b90088038505
guizenifarouk <guizeni.farouk95@gmail.com>
2019-11-06 09:51:06 +0100

    warmup 16 not finished

--- warmUp16.js
+++ warmUp16.js
@@ -16,3 +16,31 @@
 // Constraints:
 // 1 <= text.length <= 10^4
 // text consists of lower case English letters only.
+function instances(string ) {
+	var array = string.split('');						// i splited the string to an array
+	var arr = [];
+	var result;
+	for(var i = 0 ; i < array.length; i++) {			// i wanted to check if ther are any letters appearing many times
+		for(var j = 1 ; j < array.length-1 ; j++) {
+			if(array[i] === array[j]){
+			arr.push(array[i]);							// if there is we push one of them to an array and we splice them both from the original array
+			array.slice(i, 1);	
+			array.slice(j, 1);							// then i compare the length of the new array with the length of the old array and i interpret the result 
+			}
+			if (string.length !== arr.length) {
+				result = 0 ;
+			}
+			if (string.length === arr.length) {
+				result = 1;
+			}
+			if(arr.length === (2*string.length)) {
+				result = 2;
+			}
+//console.log(arr)
+//console.log(array)
+
+		}
+			
+	}
+	return result;
+} 
